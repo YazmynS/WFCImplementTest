141838c65cbb15beec87ba63061027c6d6021896
Gigz <gigzpumpking@gmail.com>
2024-10-15 16:01:20 -0700

    Committing to triple maps, added a fourth map to cover decor placements, changed noise sample value from 100 to 10 and made the value adjustable in scene

--- src/main.js
+++ src/main.js
@@ -27,3 +27,67 @@ let h = game.config.height;
 
 let centerX = game.config.width / 2;
 let centerY = game.config.height / 2;
+
+const tileSize = 64;
+const gap = 17;
+const blank = -1;
+
+const landArr = [5, 6, 7];
+const landOffset = 0;
+
+const mountainArr = [5, 6, 7];
+const mountainOffset = 4;
+
+const landDecorArr = [8, 9];
+const landDecorOffset = 2;
+
+const mountainDecorArr = [8, 9];
+const mountainDecorOffset = 6;
+
+let worldSyms = ['~', '-', '^'];
+
+let noiseSampleWindow = 10;
+
+const lookup = [
+    [1, 1],
+    [2, 1],
+    [0, 0],
+    [1, 0],
+    [0, 0],
+    [2, 0], // bottom-left
+    [0, 0], // top-left
+    [1, 0], // middle-left
+    [2, 2],
+    [2, 2], // bottom-right
+    [0, 2], // top-right
+    [1, 2], // middle-right
+    [0, 1],
+    [2, 1], // bottom-middle
+    [0, 1], // top-middle
+    [1, 1]  // floor
+];
+
+const decorPlacementLookup = [
+    true,
+    false,
+    true,
+    true,
+    true,
+    false, // bottom-left
+    true, // top-left
+    true, // middle-left
+    false,
+    false, // bottom-right
+    true, // top-right
+    true, // middle-right
+    true,
+    false, // bottom-middle
+    true, // top-middle
+    true  // floor
+];
+
+let waterThreshold = 85;
+
+let landThreshold = 170;
+
+let mountainThreshold = 255;
--- src/scenes/Map.js
+++ src/scenes/Map.js
@@ -4,48 +4,37 @@ class Map extends Phaser.Scene {
     }
 
     create() {
-        const lookup = [
-            null,
-            null,
-            null,
-            null,
-            null,
-            [2, 0], // bottom-left
-            [0, 0], // top-left
-            [1, 0], // middle-left
-            null,
-            [2, 2], // bottom-right
-            [0, 2], // top-right
-            [1, 2], // middle-right
-            null,
-            [2, 1], // bottom-middle
-            [0, 1], // top-middle
-            [1, 1]  // floor
-        ];
-
-        noise.seed(Math.random());
-
-        this.gap = 17;
+        this.reseed();
 
         this.scaleFactor = 0.5;
 
         this.width = (20 * widthFactor) / this.scaleFactor;
         this.height = (15 * heightFactor) / this.scaleFactor;
-
-        let water = [186, 202, 203];
-        let blank = -1;
-
-        let landArr = [5, 6, 7];
-        let landOffset = 0;
-        let land = this.rectangularSlice(landArr, 3, landOffset);
-
-        let mountainArr = [5, 6, 7];
-        let mountainOffset = 4;
-        let mountain = this.rectangularSlice(mountainArr, 3, mountainOffset);
+        
+        const water = [186, 202, 203];
+        const land = this.rectangularSlice(landArr, 3, landOffset);
+        const mountain = this.rectangularSlice(mountainArr, 3, mountainOffset);
 
         this.world = [water, land, mountain];
-        let worldSyms = ['~', '-', '^'];
-        
+
+        this.landDecor = this.rectangularSlice(landDecorArr, 2, landDecorOffset);
+
+        this.mountainDecor = this.rectangularSlice(mountainDecorArr, 2, mountainDecorOffset);
+
+        this.generateMap();
+
+        this.reload = this.input.keyboard.addKey('R');
+
+        this.decreaseNoise = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.COMMA);
+        this.increaseNoise = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PERIOD);
+
+        // Generate Text top-left corner to display noiseSampleWindow
+        this.noiseSampleWindowText = this.add.text(10, 10, 'Noise Sample Window: ' + noiseSampleWindow, { fontFamily: 'Arial', fontSize: '24px', color: '#000000' });
+        // Set depth to 1 so it appears above the map
+        this.noiseSampleWindowText.setDepth(2);
+    }
+
+    generateMap() {
         // arr determines tile types based on noise values
         const arr = [];
 
@@ -87,38 +76,38 @@ class Map extends Phaser.Scene {
 
                 let type = this.tileType(char);
 
-                let code = type == land ? this.tileCode(landSpecifics, i, j, char) : this.tileCode(arr, i, j, char);
+                let code = type == this.world[1] ? this.tileCode(landSpecifics, i, j, char) : this.tileCode(arr, i, j, char);
+
+                let tile;
 
                 if (lookup[code] === null) {
-                    waterLayer[i].push(this.threeSection(this.noiseValue(i, j), water));
-                    landLayer[i].push(blank);
-                    mountainLayer[i].push(blank);
-                    continue;
+                    tile = type !== this.world[0] ? type[1][1] : this.calculateWaterTile(i, j);
+                } else {
+                    let [x, y] = lookup[code];
+
+                    tile = type !== this.world[0] ? type[y][x] : this.calculateWaterTile(i, j);
                 }
 
-                let [x, y] = lookup[code];
-
-                let tile = type !== water ? type[y][x] : this.threeSection(this.noiseValue(i, j), water);
-
-                if (type == land) {
+                if (type == this.world[1]) {
                     landLayer[i].push(tile);
                     mountainLayer[i].push(blank);
-                } else if (type == mountain) {
-                    landLayer[i].push(land[1][1]);
+                } else if (type == this.world[2]) {
+                    let [landX, landY] = lookup[this.tileCode(landSpecifics, i, j, landSpecifics[i, j])];
+                    landLayer[i].push(this.world[1][landY][landX]);
                     mountainLayer[i].push(tile);
                 } else {
                     landLayer[i].push(blank);
                     mountainLayer[i].push(blank);
                 }
 
-                waterLayer[i].push(this.threeSection(this.noiseValue(i, j), water));
+                waterLayer[i].push(this.calculateWaterTile(i, j));
             }
         }
 
         const waterMap = this.make.tilemap({
             data: waterLayer,      // load direct from array
-            tileWidth: 64,
-            tileHeight: 64
+            tileWidth: tileSize,
+            tileHeight: tileSize
         })
 
         const tilesheet = waterMap.addTilesetImage('tiles');
@@ -129,8 +118,8 @@ class Map extends Phaser.Scene {
 
         const landMap = this.make.tilemap({
             data: landLayer,
-            tileWidth: 64,
-            tileHeight: 64
+            tileWidth: tileSize,
+            tileHeight: tileSize
         })
 
         const landTilesheet = landMap.addTilesetImage('tiles');
@@ -141,8 +130,8 @@ class Map extends Phaser.Scene {
 
         const mountainMap = this.make.tilemap({
             data: mountainLayer,
-            tileWidth: 64,
-            tileHeight: 64
+            tileWidth: tileSize,
+            tileHeight: tileSize
         })
 
         const mountainTilesheet = mountainMap.addTilesetImage('tiles');
@@ -151,32 +140,79 @@ class Map extends Phaser.Scene {
 
         mlayer.setScale(this.scaleFactor);
 
-        this.reload = this.input.keyboard.addKey('R')
+        this.decorateMap(arr);
+    }
+
+    reseed() {
+        this.seed = Math.random();
+        noise.seed(this.seed);
     }
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(this.reload)) {
-            this.scene.restart()
+            this.reseed();
+            this.generateMap();
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.decreaseNoise)) {
+            // Decrease noise sample window by 1
+            if (noiseSampleWindow > 1) {
+                noiseSampleWindow -= 1;
+            }
+            this.noiseSampleWindowText.text = 'Noise Sample Window: ' + noiseSampleWindow;
+            this.generateMap();
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.increaseNoise)) {
+            // Increase noise sample window by 1
+            noiseSampleWindow += 1;
+            this.noiseSampleWindowText.text = 'Noise Sample Window: ' + noiseSampleWindow;
+            this.generateMap();
         }
     }
 
     noiseValue(i, j) {
-        let value = noise.simplex2(i / 100, j / 100);
+        let value = noise.simplex2(i / noiseSampleWindow, j / noiseSampleWindow);
         return Math.round(Math.abs(value) * 256);
     }
 
-    threeSection(noiseValue, vals) {
+    threeSection(noiseValue, vals, cutoff1 = 85, cutoff2 = 170) {
         // Given a noiseValue and an array of three values,
         // returns the value from the array at an index determined by splitting the total noiseValue range into three sections.
 
         // Note: the lower the noiseValue, the lower in depth the tile will be.
 
-        if (noiseValue < 85) {
+        if (noiseValue < cutoff1) {
             return vals[0];
-        } else if (noiseValue < 170) {
+        } else if (noiseValue < cutoff2) {
             return vals[1];
         } else {
             return vals[2];
+        }
+    }
+
+    fourSection(noiseValue, vals, min, max) {
+        // Given a noiseValue and a matrix of four values,
+        // returns the value from the array at an index determined by splitting the total noiseValue range into three sections, or nothing if below a set cutoff
+
+        // Note: the lower the noiseValue, the lower in depth the tile will be.
+        
+        let diff = (max - min) / 2;
+        let cutoff = min + diff;
+        let cutoff1 = cutoff + (diff / 4);
+        let cutoff2 = cutoff + (diff / 2);
+        let cutoff3 = cutoff + 3 * (diff / 4)
+
+        if (noiseValue < cutoff) {
+            return blank;
+        } else if (noiseValue < cutoff1) {
+            return vals[0][0];
+        } else if (noiseValue < cutoff2) {
+            return vals[0][1];
+        } else if (noiseValue < cutoff3) {
+            return vals[1][0];
+        } else {
+            return vals[1][1];
         }
     }
 
@@ -195,7 +231,7 @@ class Map extends Phaser.Scene {
         for (let y = 0; y < height; y++) {
             array.push([]);
             for (let x = 0; x < width; x++) {
-                array[y].push(arr[y] + this.gap * (x + verticalGap));
+                array[y].push(arr[y] + gap * (x + verticalGap));
             }
         }
 
@@ -241,5 +277,50 @@ class Map extends Phaser.Scene {
 
     tileCheck(arr, i, j, target) {
         return arr[i] && arr[i][j] === target;
+    }
+
+    calculateWaterTile(i, j) {
+        return this.threeSection(this.noiseValue(i, j), this.world[0], waterThreshold / 3, 2*(waterThreshold / 3));
+    }
+
+    decorateMap(arr) {
+        let decorLayer = [];
+        
+        for (let i = 0; i < this.height; i++) {
+            decorLayer.push([]);
+            for (let j = 0; j < this.width; j++) {
+                switch (arr[i][j]) {
+                    case '~':
+                        decorLayer[i].push(blank);
+                        break;
+                    case '-':
+                        let placeLandDecor = decorPlacementLookup[this.tileCode(arr, i, j, '-')];
+                        let landTile = placeLandDecor ? this.fourSection(this.noiseValue(i, j), this.landDecor, waterThreshold, landThreshold) : blank;
+                        decorLayer[i].push(landTile);
+                        break;
+                    case '^':
+                        let placeMountainDecor = decorPlacementLookup[this.tileCode(arr, i, j, '^')];
+                        let mountainTile = placeMountainDecor ? this.fourSection(this.noiseValue(i, j), this.mountainDecor, landThreshold, mountainThreshold) : blank;
+                        decorLayer[i].push(mountainTile);
+                        break;
+                    default:
+                        decorLayer[i].push(blank);
+                        console.log('Invalid character. Defaulting to water tile.');
+                        break;
+                }
+            }
+        }
+
+        const decorMap = this.make.tilemap({
+            data: decorLayer,      // load direct from array
+            tileWidth: tileSize,
+            tileHeight: tileSize
+        })
+
+        const tilesheet = decorMap.addTilesetImage('tiles');
+
+        const layer = decorMap.createLayer(0, tilesheet, 0, 0);
+
+        layer.setScale(this.scaleFactor);
     }
 }
